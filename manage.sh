bcddc1bfc08a828f264609e314fced9d23b1cd1b
Joseph Edwards VIII <jedwards8th@gmail.com>
2016-01-06 22:48:13 -0500

    moved install to manage

--- manage.sh
+++ manage.sh
@@ -5,7 +5,7 @@
 readonly DEBUG=0
 readonly MAN_HELP=1    # 0=text help, 1=man help
 readonly MAN_TYPE=1    # a command by man standards
-readonly VERSION="1.0"
+readonly VERSION="0.3"
 
 # Author metadata
 readonly AUTHOR="Joseph Edwards VIII"
@@ -15,20 +15,12 @@ readonly AUTHOR_EMAIL="jedwards8th@gmail.com"
 ####  SELF  ##########################################################
 
 # The 'self' command. Has special status.
-#   (a) like `cp -r SRC DEST` - the script IS the command
-#   (b) like `git -b checkout BRANCH` - the script RUNS command
 
 # using man so prefix '-' with an escape '\'
-readonly SELF_NAME="${SCRIPT} \- CLI shell scripting for lazy people"
+readonly SELF_NAME="${SCRIPT} \- install mandelbrat or dependencies"
 
 # script summary long as you like
-readonly SELF_HELP="A skeletal wireframe to speed development of command-line
-interface (CLI) executable bash scripts. Intended to speed coding for
-more than the simplest command-line scripts. Use whenever 'optargs'
-isn't enough, or when you want to use GNU style long arguments, or
-when the number of parameters is too unwieldy for positional
-arguments, or when the script(s) you're writing are already complex,
-or when you don't just feel like doing CL argument parsing."
+readonly SELF_HELP="Install mandelbrat or just the dependencies."
 
 # SELF_ARGS - if COMMANDS defined, then define empty: `SELF_ARGS=`
 #SELF_ARGS="DEST POOP CRAP"
@@ -41,11 +33,11 @@ SELF_OPTS_HELP=( \
     )
 
 # SELF_OPTS - pairs short-long OPTS (ie, "t target" == -t, --target)
-SELF_OPTS="f foo"
-SELF_OPTS_HELP[f]="example self-as-command option"
+#SELF_OPTS="f foo"
+#SELF_OPTS_HELP[f]="example self-as-command option"
 
 # SELF_OPTARGS - string of positional option arguments
-SELF_OPTARGS[f]="BAR BAZ"
+#SELF_OPTARGS[f]="BAR BAZ"
 
 
 ####  COMMANDS  ######################################################
@@ -53,44 +45,28 @@ SELF_OPTARGS[f]="BAR BAZ"
 # Command mode: first parameter commands can have ARGS and OPTS.
 # If not defining commands then define empty: `COMMANDS=`
 
-COMMANDS=( "skel" "fling" )    # array of strings
+COMMANDS=( "install" "uninstall" "reinstall" )
 
-# (1) 'skel' command with one arg, no opts
+CMD_HELP[install]="install app or resources"
+CMD_OPTS[install]="d dep-only"
+CMD_OPTS_HELP[d]="install non-Pythonic system dependencies only"
 
-CMD_HELP[skel]="create skeleton project in directory DEST"
-CMD_ARGS[skel]="DEST"
-
-# (2) 'fling' command with two args, one opt -- a dummy command
-
-CMD_HELP[fling]="fling love from SRC at DEST"
-CMD_ARGS[fling]="SRC DEST"    # space-separated string of positional args
-CMD_OPTS[fling]="p poo t toss"    # string of pairwise short-long opts
-
-# (2.1) if CMD_OPTS[CMD] defined, optionally define optargs w opt keys
-
-CMD_OPTARGS[p]="POO BAH"         # option args space-separated string
-CMD_OPTS_HELP[p]="SRC instead flings POO at DEST"
-CMD_OPTS_HELP[t]="SRC instead tosses at DEST"
+CMD_HELP[uninstall]="uninstall app only"
+CMD_HELP[reinstall]="reinstall app only"
 
 
 ####  OPTIONAL MANPAGE  ##############################################
 
-readonly FILES="The 'skel' command copies files in './skel' directory (or the
-current scripts) to the given destination path, renaming them in the
-process."
-
+readonly FILES=
 readonly ENVIRONMENT=
-
 readonly EXIT_STATUS="Exits with status \$ERRNO."
 
-readonly EXAMPLE_01="${SCRIPT} skel /path/to/new/script"
-readonly EXAMPLE_02="${SCRIPT} fling -t -p poo dirt
-\(rs\"Phil Collins\(rs\" \(rs\"Batman and Robin\(rs\""
-readonly EXAMPLES="\&${EXAMPLE_01}\n\n\&${EXAMPLE_02}"
+#readonly EXAMPLE_01="${SCRIPT} skel /path/to/new/script"
+#readonly EXAMPLE_02="${SCRIPT} fling -t -p poo dirt
+#\(rs\"Phil Collins\(rs\" \(rs\"Batman and Robin\(rs\""
+#readonly EXAMPLES="\&${EXAMPLE_01}\n\n\&${EXAMPLE_02}"
 
-readonly BUGS="Quotes must be escaped for multiple word arguments. Only double
-quotes are supported."
-
+readonly BUGS=
 readonly SEE_ALSO=
 
 
@@ -106,7 +82,6 @@ function _run_self {
     _str_equal "$CMD" "self" && self_as_command && _exit_err
 }
 
-# example of self-as-command
 function self_as_command {
 
     if ! _is_empty "${ARGS[@]}"; then
@@ -129,12 +104,6 @@ function self_as_command {
                 echo "$SCRIPT $VERSION"
                 _exit_err
                 ;;
-            f)
-                # NOTICE the use of 'eval' to get OPTARGS array
-                eval fopts=( ${OPTARGS[f]} )
-                echo "BAR: ${fopts[0]}"
-                echo "BAZ: ${fopts[1]}"
-                ;;
             *)
                 _print_help
                 _err; _exit_err
@@ -142,6 +111,11 @@ function self_as_command {
         esac
     done
 }
+
+
+
+
+####  COMMAND FUNCTIONS  #############################################
 
 # copy the skel directory and files to chosen destination
 function copy_skel {
